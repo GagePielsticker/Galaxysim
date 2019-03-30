b4d073bfab14f2f3e10d22af778cea4b6b96e164
James <gagepays@gmail.com>
2019-03-30 01:35:32 -0500

    Another upgrade bug fix...

--- src/bot/commands/game/upgrade.js
+++ src/bot/commands/game/upgrade.js
@@ -40,7 +40,7 @@ module.exports.load = client => {
                 }
 
                 if(args[0] == 'attack'){
-                    let price = res.ship.scanner_strength * client.settings.game.att_upgrade_cost
+                    let price = res.ship.att * client.settings.game.att_upgrade_cost
                     if(res.credits - price < 0) return client.send_error(message, `You aren\'t able to afford this upgrade. This will cost you \`${price}\` credits.`)
                     vericheck('attack', price)
                     client.cooldowns.collector.push(message.author.id)
@@ -68,7 +68,7 @@ module.exports.load = client => {
                 }
 
                 if(args[0] == 'scan'){
-                    let price = res.ship.att * client.settings.game.scan_upgrade_cost
+                    let price = res.ship.scanner_strength * client.settings.game.scan_upgrade_cost
                     if(res.credits - price < 0) return client.send_error(message, `You aren\'t able to afford this upgrade. This will cost you \`${price}\` credits.`)
                     if(res.ship.scanner_strength + client.settings.game.scan_upgrade_amount > res.ship.max_scanner_strength) return client.send_error(message, `Your ship type \`${res.ship.type}\` cant fit any more scan upgrades.`)
                     vericheck('scan', price)
