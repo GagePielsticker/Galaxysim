57d66702e44272fa4e0d3a795b8acbdaa655ac64
James <gagepays@gmail.com>
2019-05-05 18:03:01 -0500

    Handled message edit event

--- bot/lib/events.js
+++ bot/lib/events.js
@@ -114,4 +114,40 @@ module.exports = client => {
         //Attempt to send message
         channel.send(message)
     })
+
+    /**
+     * Handles message edit event
+     * @param {Object} oldMessage discord message
+     * @param {Object} newMessage discord message
+     */
+    client.on('messageUpdate', async (oldMessage, newMessage) => {
+
+        //If content was changed
+        if(oldMessage.content != newMessage.content) {
+
+            //load guild entry
+            let g = await client.db.collection('guilds').findOne({id:newMessage.guild.id})
+
+            //Check if the toggle is turned on
+            if(!g.chatLogToggle) return
+            
+            //Check if they have set a channel
+            if(g.chatLogChannel == '') return
+
+            //Get channel
+            let channel = await member.guild.channels.get(g.chatLogChannel)
+
+            //Format log message
+            let embed = new client.discord.MessageEmbed()
+            .setTitle('Message Edited')
+            .addField('Old', `\`\`\`${oldMessage.content} \`\`\``)
+            .addfield('New', `\`\`\`${newMessage.content} \`\`\``)
+            .setFooter(`${newMessage.user.tag}`, newMessage.author.avatarURL)
+            .setTimestamp()
+            .setColor(client.settings.embedColor)
+
+            //send message
+            await message.channel.send(embed)
+        }
+    })
 }
