807a87b597e0813a520e2b66713813379829e572
James <gagepays@gmail.com>
2019-05-05 14:26:25 -0500

    Added welcome and leave toggles

--- bot/lib/moderation.js
+++ bot/lib/moderation.js
@@ -30,7 +30,9 @@ module.exports = client => {
             let newGuild = {
                 id: id,
                 welcomeMessage: '',
-                leaveMessage: ''
+                leaveMessage: '',
+                welcomeToggle: false,
+                leaveToggle: false
             }
 
             //write guild to database
@@ -102,6 +104,54 @@ module.exports = client => {
             
             //set welcome message to string in database
             client.db.collection('guilds').updateOne({id:id}, {$set:{leaveMessage:string}}, {upsert:true})
+
+            //resolve
+            resolve()
+        })
+    }
+
+    /**
+     * Set the leave toggle in guild database
+     * @param {String} id discord guild id
+     * @param {Boolean} bool Leave message string
+     * @param {String} executor Executor of function
+     * @returns {Promise}
+     */
+    set.leaveToggle = (id, bool, executor) => {
+        return new Promise(async (resolve, reject) => {
+
+            //load guild
+            let guild = await client.db.collection('guilds').findOne({id: id})
+
+            //check if executor has administrator permissions
+            if(!mod.hasPerm(id, executor, 'ADMINISTRATOR')) return reject('User is not administrator.')
+            
+            //set welcome message to string in database
+            client.db.collection('guilds').updateOne({id:id}, {$set:{leaveToggle:bool}}, {upsert:true})
+
+            //resolve
+            resolve()
+        })
+    }
+
+    /**
+     * Set the welcome toggle in guild database
+     * @param {String} id discord guild id
+     * @param {Boolean} bool Leave message string
+     * @param {String} executor Executor of function
+     * @returns {Promise}
+     */
+    set.welcomeToggle = (id, bool, executor) => {
+        return new Promise(async (resolve, reject) => {
+
+            //load guild
+            let guild = await client.db.collection('guilds').findOne({id: id})
+
+            //check if executor has administrator permissions
+            if(!mod.hasPerm(id, executor, 'ADMINISTRATOR')) return reject('User is not administrator.')
+            
+            //set welcome message to string in database
+            client.db.collection('guilds').updateOne({id:id}, {$set:{welcomeToggle:bool}}, {upsert:true})
 
             //resolve
             resolve()
