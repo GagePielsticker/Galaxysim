ccc5f3e068ebb225873babed2fa6f41fe7254933
James <gagepays@gmail.com>
2019-05-05 14:13:35 -0500

    Added leave message setting

--- bot/lib/moderation.js
+++ bot/lib/moderation.js
@@ -29,7 +29,8 @@ module.exports = client => {
             //setup default guild object for database
             let newGuild = {
                 id: id,
-                welcomeMessage: ''
+                welcomeMessage: '',
+                leaveMessage: ''
             }
 
             //write guild to database
@@ -74,6 +75,33 @@ module.exports = client => {
             
             //set welcome message to string in database
             client.db.collection('guilds').updateOne({id:id}, {$set:{welcomeMessage:string}}, {upsert:true})
+
+            //resolve
+            resolve()
+        })
+    }
+
+    /**
+     * Set the leave message in guild database
+     * @param {String} id discord guild id
+     * @param {String} string Leave message string
+     * @param {String} executor Executor of function
+     * @returns {Promise}
+     */
+    set.leaveMessage = (id, string, executor) => {
+        return new Promise(async (resolve, reject) => {
+
+            //load guild
+            let guild = await client.db.collection('guilds').findOne({id: id})
+
+            //check if string is empty
+            if(string.length == 0) return reject('You supplied an empty string.')
+
+            //check if executor has administrator permissions
+            if(!mod.hasPerm(id, executor, 'ADMINISTRATOR')) return reject('User is not administrator.')
+            
+            //set welcome message to string in database
+            client.db.collection('guilds').updateOne({id:id}, {$set:{leaveMessage:string}}, {upsert:true})
 
             //resolve
             resolve()
