2e10a48e50fa118f2fd713258dc5a3b8909b4195
James <gagepays@gmail.com>
2019-05-05 20:29:35 -0500

    Fixed help to support admin stuff

--- bot/commands/moderation/settings.js
+++ bot/commands/moderation/settings.js
@@ -3,7 +3,16 @@ module.exports.load = client => {
         settings : {
             type : 'moderation',
             description : 'test',
-            usage : `${client.settings.prefix}test`
+            usage : `${client.settings.prefix}settings welcomes {on/off}
+            ${client.settings.prefix}settings leaves {on/off}
+            ${client.settings.prefix}settings welcome_message {string}
+            ${client.settings.prefix}settings leave_message {string}
+            ${client.settings.prefix}settings welcome_channel {<#channel_id>}
+            ${client.settings.prefix}settings leave_channel {<#channel_id>}
+            ${client.settings.prefix}settings chatlog {on/off}
+            ${client.settings.prefix}settings chatlog_channel {<#channel_id>}
+            ${client.settings.prefix}settings autorole {on/off}
+            ${client.settings.prefix}settings autorole_role {<@role_id>}`
         },
 
         async run(message) {
